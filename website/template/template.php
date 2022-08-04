1676e09fd2cac7dff5292f911eb313e4ea12129c
varun777k <31365602+varun777k@users.noreply.github.com>
2022-08-04 18:36:28 +0530

    Update template.php

--- website/template/template.php
+++ website/template/template.php
@@ -5,7 +5,7 @@
     <title><?php pageTitle(); ?> | <?php siteName(); ?></title>
     <style type="text/css">
         .wrap { max-width: 720px; margin: 50px auto; padding: 30px 40px; text-align: center; box-shadow: 0 4px 25px -4px #9da5ab; }
-        article { text-align: left; padding: 40px; line-height: 150%; }
+        article {  text-align: left; padding: 40px; line-height: 150%; }
     </style>
 </head>
 <body>
